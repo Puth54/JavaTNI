054f38148a330d64a937cc5c51df0b6a38054e9b
Puth54 <mezt59@gmail.com>
2025-11-25 13:12:55 +0700

    ณัฐเมศร์ Lab Sheet 01

---
+++ Labsheet1/src/Lab101.java
@@ -0,0 +1,7 @@
+public class Lab101 {
+    public static void main(String[] args) {
+        System.out.println("I am Naththamezt Leardchaiwattanakual");
+        System.out.println("\tThis is my first time to learn  Java");
+        System.out.println("\tI hope to get a grade \"A\"");
+    }
+}
