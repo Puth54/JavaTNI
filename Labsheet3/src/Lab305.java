9a27a691818d0d7418b527730e883ef043968a80
Puth54 <mezt59@gmail.com>
2025-12-02 13:19:39 +0700

    ณัฐเมศร์ 2513510129

--- Labsheet3/src/Lab305.java
+++ Labsheet3/src/Lab305.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class ศฟิ305 {
+public class Lab305 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int b = sc.nextInt();
+
+        int start = Math.min(a, b);
+        int end = Math.max(a, b);
+
+        for (int i = start; i <= end; i++) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+    }
 }
--- Labsheet3/src/Lab307.java
+++ Labsheet3/src/Lab307.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
 public class Lab307 {
+    public static void main(String[] args) {
+
+        double price, total = 0;
+
+        while (true) {
+            String input = JOptionPane.showInputDialog("Input price (press 0 to stop):");
+            price = Double.parseDouble(input);
+
+            if (price < 0) {
+                JOptionPane.showMessageDialog(null, "Invalid price!!");
+                continue;
+            }
+
+            if (price == 0) {
+                break;
+            }
+            total += price;
+        }
+
+        JOptionPane.showMessageDialog(null,
+                "Total price is " + total + "baht");
+    }
 }
